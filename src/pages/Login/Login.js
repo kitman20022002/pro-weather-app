5a50ac31a6b2209e6b60c7471dc2c6fb24066088
kitman20022002 <kitman200220022002@gmail.com>
2020-11-29 09:38:21 +1100

    asdded get location api

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -56,6 +56,26 @@ class Login extends React.Component {
         };
         this.loadDefaultData();
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+        let options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+
+        navigator.geolocation.getCurrentPosition(this.success , this.error, options)
+    }
+
+    success(pos) {
+        var crd = pos.coords;
+
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+    }
+
+    error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
     }
 
     componentDidMount() {
