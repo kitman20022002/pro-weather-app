1cf4a48094628d54a4fd3f9c4e33badb642bcbc3
kitman20022002 <kitman200220022002@gmail.com>
2021-08-02 17:39:19 +1000

    added file uploader

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+ extends: ['airbnb', 'prettier'],
+ plugins: ['prettier'],
+ parser: 'babel-eslint',
+ parserOptions: {
+   sourceType: 'module',
+   allowImportExportEverywhere: false,
+   codeFrame: false,
+ },
+ env: {
+   browser: true,
+   jest: true,
+ },
+ rules: {
+   'prettier/prettier': ['error'],
+   'no-console': 2,
+   'no-debugger': 'error',
+   'no-alert': 'error',
+   'default-case': 'error',
+   'max-len': [
+     'error',
+     {
+       code: 120,
+     },
+   ],
+   'prefer-promise-reject-errors': ['off'],
+   'react/jsx-filename-extension': ['off'],
+   'react/prop-types': ['off'],
+   'no-return-assign': ['off'],
+ },
+};
