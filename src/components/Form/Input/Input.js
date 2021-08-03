a2d8a8951d54d3b4cd19c60728130d01e8e9b906
kitman20022002 <kitman200220022002@gmail.com>
2021-08-03 10:38:44 +1000

    fix format issues

--- src/components/Form/Input/Input.js
+++ src/components/Form/Input/Input.js
@@ -1,26 +1,23 @@
 import React from 'react';
 
-class Input extends React.Component {
-  render() {
-    const { type, name, label, placeholder, valid, onChange, error } = this.props;
-    const { value, cssClass } = this.state;
-    const extraAtt = type === 'password' ? 'on' : 'off';
-    return (
-      <div className="flex flex__column form--group">
-        <label htmlFor={name}>{label}: </label>
-        <input
-          name={name}
-          type={type}
-          placeholder={placeholder}
-          value={value}
-          className={[cssClass, +!valid ? 'error-input' : ''].join(' ')}
-          onChange={onChange}
-          autoComplete={extraAtt}
-        />
-        {!valid && <p className="color--red error-message">{error}</p>}
-      </div>
-    );
-  }
+function Input(props) {
+  const { type, name, label, placeholder, valid, onChange, error, cssClass, value } = props;
+  const extraAtt = type === 'password' ? 'on' : 'off';
+  return (
+    <div className="flex flex__column form--group">
+      <label htmlFor={name}>{label}: </label>
+      <input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        className={[cssClass, +!valid ? 'error-input' : ''].join(' ')}
+        onChange={onChange}
+        autoComplete={extraAtt}
+      />
+      {!valid && <p className="color--red error-message">{error}</p>}
+    </div>
+  );
 }
 
 export default Input;
